402134bc65854cf152f916457db9d4eb4a037487
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-09-29 11:00:34 +0300

    Setup association between student and classroom

--- student.rb
+++ student.rb
@@ -6,7 +6,12 @@ class Student < Person
     @classroom = classroom
   end
 
-  attr_accessor :classroom
+  attr_reader :classroom
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
+  end
 
   def play_hooky
     "¯\(ツ)/¯"
