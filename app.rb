a835661fa43e1bc67d3cdce19b553df48f7b95a4
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-10-05 14:17:04 +0300

    Persist Book and People data on files

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative 'student'
 require_relative 'teacher'
 require_relative 'book'
@@ -31,6 +32,7 @@ class App
     author = gets.chomp
 
     book = Book.new(title, author)
+    save_book(book)
     @books.push(book)
     puts 'Book created successfully'
   end
@@ -73,6 +75,60 @@ class App
     person.rentals.each_with_index { |rental, i| puts "#{i}) Book: #{rental.book.title}, Date: #{rental.date}" }
   end
 
+  def save_book(book)
+    if File.exist?('books.json')
+      books_loaded = JSON.parse(File.read('books.json'))
+      books_loaded << { title: book.title, author: book.author }
+      File.write('books.json', JSON.pretty_generate(books_loaded))
+    else
+      File.write('books.json', JSON.pretty_generate([{ title: book.title, author: book.author }]))
+    end
+  end
+
+  def load_books
+    return unless File.exist?('books.json')
+
+    books_loaded = JSON.parse(File.read('books.json'))
+    books_loaded.each do |book|
+      new_book = Book.new(book['title'], book['author'])
+      @books << new_book
+    end
+  end
+
+  def save_person(person)
+    new_person = nil
+    if person.instance_of? Student
+      new_person = { id: person.id, age: person.age, name: person.name, parent_permission: person.parent_permission,
+                     type: 'student' }
+    elsif person.instance_of? Teacher
+      new_person = { id: person.id, age: person.age, name: person.name, specialization: person.specialization,
+                     type: 'teacher' }
+    end
+    if File.exist?('people.json')
+      people_loaded = JSON.parse(File.read('people.json'))
+      people_loaded << new_person
+      File.write('people.json', JSON.pretty_generate(people_loaded))
+    else
+      File.write('people.json', JSON.pretty_generate([new_person]))
+    end
+  end
+
+  def load_people
+    return unless File.exist?('people.json')
+
+    people_loaded = JSON.parse(File.read('people.json'))
+    people_loaded.each do |person|
+      case person['type']
+      when 'student'
+        new_person = Student.new(nil, person['id'], person['age'], person['name'], person['parent_permission'])
+        @people << new_person
+      when 'teacher'
+        new_person = Teacher.new(person['specialization'], person['id'], person['age'], person['name'])
+        @people << new_person
+      end
+    end
+  end
+
   private
 
   def create_student
@@ -83,7 +139,8 @@ class App
     print 'Has parent permission? [Y/N]: '
     permission = gets.chomp.downcase == 'y'
 
-    student = Student.new(nil, age, name, permission)
+    student = Student.new(nil, nil, age, name, permission)
+    save_person(student)
     @people.push(student)
     puts 'Person created successfully'
   end
@@ -96,7 +153,8 @@ class App
     print 'Specialization: '
     specialization = gets.chomp
 
-    teacher = Teacher.new(specialization, age, name)
+    teacher = Teacher.new(specialization, nil, age, name)
+    save_person(teacher)
     @people.push(teacher)
     puts 'Person created successfully'
   end
